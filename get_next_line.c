40c4616936e7bb24d46bd6c69964389916cbdd71
Marlena <marlena.snitko.wiecek@gmail.com>
2024-04-18 00:25:21 +0200

    cutting lines

--- get_next_line.c
+++ get_next_line.c
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
 
 #ifndef BUFFER_SIZE
-#define BUFFER_SIZE 100
+#define BUFFER_SIZE 1
 #endif
 
 #include <stdlib.h>
@@ -27,21 +27,23 @@ struct Node
 	struct Node* next;
 };
 /* I add new node to the existing list */
-void append(struct Node* head, char *str)
+struct Node* append(struct Node* head, char *str)
 {
 	struct Node	*new_node;
 	struct Node	*current_node;
 
 	new_node = (struct Node*)malloc(sizeof(struct Node));
-	if (new_node == NULL)
-		return;
-	current_node = head;
-	while (current_node->next != NULL) {
-		current_node = current_node->next;
-	}
+	if (new_node == NULL) 
+		return (NULL);
 	new_node->data = ft_strdup(str);
 	new_node->next = NULL;
+	if (head == NULL)
+		return (new_node);
+	current_node = head;
+	while (current_node->next != NULL)
+		current_node = current_node->next;
 	current_node->next = new_node;
+	return (head);
 }
 /* Memory allocation for next line string*/
 char	*mem_alloc(struct Node *head)
@@ -61,30 +63,26 @@ char	*mem_alloc(struct Node *head)
 		current_node = current_node->next;
 	}
 	i = 0;
-	while(current_node->data[i] != '\n' && current_node->data[i]) {
+	while(current_node->data[i] != '\n' && current_node->data[i])
 		i++;
-	}
 	len = len + i;
 	new_line = (char *)malloc((len + 2) * sizeof(char));
 	if (new_line == NULL)
 		return (NULL);
 	return (new_line);
 }
-/*next line string creation*/
-char	*create_line(struct Node **list)
+
+int copy_nodes_to_str(struct Node** current_node_ptr, char* new_line)
 {
-	char	*new_line;
-	struct Node	*current_node;
-	int	i;
-	int	j;
-	struct Node	*new_node;
+	int i;
 	struct Node	*tmp;
-	if ((*list) == NULL) {
-		return NULL;
-	}
+
 	i = 0;
-       	new_line = mem_alloc(*list);
-	current_node = *list;
+
+	struct Node * current_node;
+
+	current_node = *current_node_ptr;
+
 	while (current_node->next)
 	{
 		ft_memcpy(new_line + i, current_node->data, ft_strlen(current_node->data));
@@ -94,34 +92,56 @@ char	*create_line(struct Node **list)
 		free(tmp->data);
 		free(tmp);
 	}
+	*current_node_ptr = current_node;
+	return (i);
+}
+
+
+int handle_last_node(struct Node** current_node_ptr, char* new_line, int i)
+{
+	int j;
+	struct Node * current_node;
+	struct Node	*new_node;
+
 	j = 0;
+	current_node = *current_node_ptr;
+
 	while(current_node->data[j] && current_node->data[j] != '\n')
-	{
-		new_line[i] = current_node->data[j];
-		j++;
-		i++;
-	}
-	if (current_node->data[j] == '\n') {
-		new_line[i] = '\n';
-		i++;
-	}
+		new_line[i++] = current_node->data[j++];
+	if (current_node->data[j] == '\n')
+		new_line[i++] = '\n';
 	new_line[i] = '\0';
-	if (current_node->data[j]) {
+	if (current_node->data[j])
+	{
 		new_node = (struct Node*)malloc(sizeof(struct Node));
 		if (new_node == NULL)
-			return (NULL);
+			return (-1);
 		new_node->data = ft_strdup(current_node->data + j + 1);
 		new_node->next = NULL;
-		*list = new_node;
+		*current_node_ptr = new_node;
 	}
 	else 
-	{
-		*list = NULL;
-	}
+		*current_node_ptr = NULL;
 	free(current_node->data);
-	free(current_node);
+	free(current_node);	
+	return 0;
+}
 
-	
+
+/*next line string creation*/
+char	*create_line(struct Node **list_ptr)
+{
+	char	*new_line;
+	int	i;
+	if ((*list_ptr) == NULL)
+		return NULL;
+       	new_line = mem_alloc(*list_ptr);
+	i = copy_nodes_to_str(list_ptr, new_line);
+	if (handle_last_node(list_ptr, new_line, i) == -1)
+	{
+		return (NULL);
+	}
+
 	return (new_line);
 }
 
@@ -130,73 +150,54 @@ int last_node_has_newline(struct Node* list)
 	struct Node	*current;
 	current = list;
 	if (!current)
-	{
-		return 0;
-	}
+		return (0);
 	while (current->next)
+		current = current->next;
+	return (ft_strchr(current->data, '\n') != NULL);
+}
+
+char * read_until_newline(int fd, struct Node ** list_ptr, char * buffer) 
+{
+	int num_chars;
+
+	while (!ft_strchr(buffer, '\n'))
 	{
-		current = current->next;	
+		num_chars = read(fd, buffer, BUFFER_SIZE);
+		buffer[num_chars] = '\0';
+
+		if (num_chars <= 0) 
+		{
+			free(buffer);
+			return create_line(list_ptr);
+		}
+		*list_ptr = append(*list_ptr, buffer);
+		if (!*list_ptr)
+		{
+			free(buffer);
+			return (NULL);
+		}
 	}
-	return ft_strchr(current->data, '\n') != NULL;
+	free(buffer);
+	return create_line(list_ptr);
 }
 
 char	*get_next_line(int fd)
 {
 	char		*buffer;
-	int		num_chars;
 	static struct Node	*list;
-	if (fd == -1) {
-		return NULL;
-	}
+	if (fd == -1)
+		return (NULL);
+	if (last_node_has_newline(list)) 
+		return create_line(&list);
 
-	if (!last_node_has_newline(list)) 
+	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (buffer == NULL) 
 	{
-		buffer = (char *)malloc((BUFFER_SIZE) * sizeof(char));
-		if (buffer == NULL) {
-			free(list);
-			return (NULL);
-		}
-		num_chars = read(fd, buffer, BUFFER_SIZE);
-		if (num_chars <= 0) {
-			free(buffer);
-			return create_line(&list);
-		}
-		char * buf2;
-		buf2 = (char*)malloc(num_chars + 1);
-		ft_memcpy(buf2, buffer, num_chars);
-		buf2[num_chars] = '\0';
-
-		if (!list)
-		{
-			list = (struct Node*)malloc(sizeof(struct Node));
-			if (list == NULL)
-			{
-				free(buffer);
-				return (NULL);
-			}
-			list->data = ft_strdup(buf2);
-			list->next = NULL;
-		}
-		else
-		{
-			append(list, buf2);
-		}
-		while (!ft_strchr(buf2, '\n'))
-		{
-			free(buf2);
-			num_chars = read(fd, buffer, BUFFER_SIZE);
-			buf2 = (char*)malloc(num_chars + 1);
-			ft_memcpy(buf2, buffer, num_chars);
-			buf2[num_chars] = '\0';
-			if (!num_chars) {
-				free(buffer);
-				free(buf2);
-				return (create_line(&list));
-			}
-			append(list, buf2);
-		}
-		free(buf2);
-		free(buffer);
+		free(list);
+		return (NULL);
 	}
-	return (create_line(&list));
+	buffer[0] = '\0';
+
+
+	return read_until_newline(fd, &list, buffer);
 }
